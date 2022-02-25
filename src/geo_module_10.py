f4dd9a2adeb16090174c1ac9e687c25adc2cd033
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-02-25 14:01:25 +0000

    refactor: improve error handling

---
+++ src/geo_module_10.py
@@ -0,0 +1,12 @@
+"""Geolocation module 10 for skytrack"""
+import numpy as np
+import torch
+from typing import List, Dict
+
+class LocationAnalyzer10:
+    def __init__(self):
+        self.model = torch.nn.Linear(100, 2)
+    
+    async def analyze(self, data: np.ndarray) -> Dict:
+        result = self.model(torch.tensor(data))
+        return {"lat": float(result[0]), "lng": float(result[1])}
