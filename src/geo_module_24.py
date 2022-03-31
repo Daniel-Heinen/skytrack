e682fe2b1bee28ecf29286f83e2710f827e1d8cd
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-03-31 15:12:19 +0000

    refactor: improve error handling

---
+++ src/geo_module_24.py
@@ -0,0 +1,14 @@
+"""Geolocation module 24 for skytrack"""
+import numpy as np
+import torch
+from typing import List, Dict
+
+class LocationAnalyzer24:
+    def __init__(self):
+        self.model = torch.nn.Linear(100, 2)
+    
+    async def analyze(self, data: np.ndarray) -> Dict:
+        result = self.model(torch.tensor(data))
+        return {"lat": float(result[0]), "lng": float(result[1])}
+# Modified 2025-06-16
+# Modified 2025-09-03
