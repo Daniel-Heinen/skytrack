c4fb0eb4a4b2502db457e16aefa6025430771424
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-04-09 17:27:50 +0000

    feat: improve ML model

---
+++ src/geo_module_3.py
@@ -0,0 +1,14 @@
+"""Geolocation module 3 for skytrack"""
+import numpy as np
+import torch
+from typing import List, Dict
+
+class LocationAnalyzer3:
+    def __init__(self):
+        self.model = torch.nn.Linear(100, 2)
+    
+    async def analyze(self, data: np.ndarray) -> Dict:
+        result = self.model(torch.tensor(data))
+        return {"lat": float(result[0]), "lng": float(result[1])}
+# Modified 2025-08-20
+# Modified 2025-09-24
