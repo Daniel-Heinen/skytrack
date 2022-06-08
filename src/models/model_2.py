7a52506c6b8a860390e1a161c7ac7c91421c874c
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-06-08 10:17:54 +0000

    fix: resolve authentication issue

---
+++ src/models/model_2.py
@@ -0,0 +1,25 @@
+"""Data model 2 for geolocation"""
+from pydantic import BaseModel, Field, validator
+from typing import Optional, List
+from datetime import datetime
+
+class GeoModel2(BaseModel):
+    id: int
+    latitude: float = Field(..., ge=-90, le=90)
+    longitude: float = Field(..., ge=-180, le=180)
+    accuracy: float
+    timestamp: datetime
+    metadata: Optional[dict] = None
+    tags: List[str] = []
+    
+    @validator("accuracy")
+    def validate_accuracy(cls, v):
+        if not 0 <= v <= 1:
+            raise ValueError("Accuracy must be between 0 and 1")
+        return v
+    
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
+# Modified 2025-06-13
