5f94b84e367804a50a087e491836fbea6ae60dcc
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-06-09 10:13:29 +0000

    chore: update dependencies

---
+++ src/models/model_3.py
@@ -0,0 +1,24 @@
+"""Data model 3 for geolocation"""
+from pydantic import BaseModel, Field, validator
+from typing import Optional, List
+from datetime import datetime
+
+class GeoModel3(BaseModel):
+    id: int
+    latitude: float = Field(..., ge=-90, le=90)
+    longitude: float = Field(..., ge=-180, le=180)
+    accuracy: float
+    timestamp: datetime
+    metadata: Optional[dict] = None
+    tags: List[str] = []
+    
+    @validator("accuracy")
+    def validate_accuracy(cls, v):
+        if not 0 <= v <= 1:
+            raise ValueError("Accuracy must be between 0 and 1")
+        return v
+    
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
