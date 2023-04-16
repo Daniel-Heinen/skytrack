29845b1726d7c716cdffa8567dce3fa28c6d8b7b
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2023-04-16 19:21:56 +0000

    refactor: restructure codebase

---
+++ tests/test_processor_81.py
@@ -0,0 +1,40 @@
+"""Comprehensive tests for processor module 81"""
+import pytest
+import numpy as np
+import torch
+from src.processor_81 import AdvancedGeoProcessor81, GeoLocation
+
+class TestGeoProcessor81:
+    
+    @pytest.fixture
+    def processor(self):
+        return AdvancedGeoProcessor81()
+    
+    def test_initialization(self, processor):
+        assert processor.input_dim == 512
+        assert processor.hidden_dim == 256
+        assert isinstance(processor, torch.nn.Module)
+    
+    def test_forward_pass(self, processor):
+        x = torch.randn(1, 512)
+        output = processor.forward(x)
+        assert output.shape == (1, 2)
+    
+    @pytest.mark.asyncio
+    async def test_predict_location(self, processor):
+        features = np.random.randn(512)
+        location = await processor.predict_location(features)
+        assert isinstance(location, GeoLocation)
+        assert -90 <= location.latitude <= 90
+        assert -180 <= location.longitude <= 180
+        assert 0 <= location.confidence <= 1
+    
+    def test_model_parameters(self, processor):
+        params = list(processor.parameters())
+        assert len(params) > 0
+    
+    def test_training_mode(self, processor):
+        processor.train()
+        assert processor.training == True
+        processor.eval()
+        assert processor.training == False
