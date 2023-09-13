8405b2b4db205df538a263edf5291b7e6de82ce1
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2023-09-13 10:18:41 +0000

    chore: update dependencies

--- src/geo_module_41.py
+++ src/geo_module_41.py
@@ -11,3 +11,4 @@ class LocationAnalyzer41:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
 # Modified 2025-11-13
+# Modified 2023-09-13
