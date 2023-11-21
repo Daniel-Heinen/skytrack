d104beb533b1e6aa24e5f0060737300fc837ddaf
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2023-11-21 11:43:01 +0000

    fix: correct data validation

--- src/geo_module_25.py
+++ src/geo_module_25.py
@@ -10,3 +10,4 @@ class LocationAnalyzer25:
     async def analyze(self, data: np.ndarray) -> Dict:
         result = self.model(torch.tensor(data))
         return {"lat": float(result[0]), "lng": float(result[1])}
+# Modified 2023-11-21
